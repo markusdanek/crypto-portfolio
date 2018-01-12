16fb33c51d1a440df3df6cc8030e92f97a83ed9d
Markus Danek <markus.danek-cic@ibm.com>
2018-01-12 16:56:01 +0100

    Add form to add coins/amount and save it to localStorage

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h1>crypto-portfolio</h1>
-    <!-- <input v-model="crypto" v-on:input="set_data" > -->
 
     <div v-for="coin in cryptocurrency">
+      <input v-model="coin.name">
       <input v-model="coin.value">
     </div>
     <button @click="addCrypto">
@@ -19,28 +19,25 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      // cryptoStorage: localStorage.getItem('cryptocurrencies'),
-      cryptocurrency: []
+      cryptocurrency: [],
     }
   },
   methods: {
   	set_data() {
-      localStorage.setItem( 'cryptocurrencies', this.cryptocurrency );
+      localStorage.setItem( 'cryptocurrencies', JSON.stringify(this.cryptocurrency) );
       this.cryptocurrency = this.get_data();
   	},
     get_data(){
-      return localStorage.getItem( 'cryptocurrencies' );
+      return localStorage.getItem('cryptocurrencies');
     },
     addCrypto() {
-      this.cryptocurrency.push({ coin: '' });
-      localStorage.setItem( 'cryptocurrencies', JSON.stringify(this.cryptocurrency));
+      localStorage.setItem( 'cryptocurrencies', JSON.stringify(this.cryptocurrency) );
+      this.cryptocurrency.push({ name: '', value: ''});
     }
   }
 }
 </script>
 
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
+
 </style>
