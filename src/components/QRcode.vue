60f5953fc802ac85d93db0871ab56ac157e8bb33
Markus Danek <markus.danek-cic@ibm.com>
2018-02-08 12:20:38 +0100

    Generate QR code for 'cryptos'

--- src/components/QRcode.vue
+++ src/components/QRcode.vue
@@ -1,28 +1,54 @@
 <template>
-  <QrcodeReader
-    @decode="onDecode"
-    @init="onInit">
-    <div class="decoded-content">{{ content }}</div>
+  <div class="">
+    <QrcodeReader
+      @decode="onDecode"
+      @init="onInit">
+      <div class="decoded-content">{{ content }}</div>
 
-    <LoadingIndicator v-show="loading" />
-  </QrcodeReader>
+      <LoadingIndicator v-show="loading" />
+    </QrcodeReader>
+
+    <VueQrcode :value="cryptosQR" :options="{ size: 600 }"></VueQrcode>
+  </div>
 </template>
 
 <script>
-  import { QrcodeReader } from 'vue-qrcode-reader'
-  import InitHandler from '@/mixins/InitHandler'
+  import { QrcodeReader } from 'vue-qrcode-reader';
+  import InitHandler from '@/mixins/InitHandler';
+  import VueQrcode from '@xkeshi/vue-qrcode';
+  import { cryptostorage } from '../helpers/utils';
+
   export default {
-    components: { QrcodeReader },
+    components: {
+      QrcodeReader,
+      VueQrcode
+    },
     mixins: [ InitHandler ],
     data () {
       return {
-        content: ''
+        content: '',
+        cryptos: cryptostorage.fetch(),
+        cryptosQR : ''
       }
     },
     methods: {
       onDecode (content) {
         this.content = content
+      },
+      convertCrypto() {
+        let test = this.cryptos;
+        test.forEach(function(line) {
+            delete line['priceToday'];
+            delete line['priceYesterday'];
+            delete line['purchaseDatePrice'];
+            delete line['purchaseValueFiat'];
+            delete line['id'];
+        });
+        this.cryptosQR = JSON.stringify(test);
       }
+    },
+    mounted() {
+      this.convertCrypto();
     }
   }
 </script>
