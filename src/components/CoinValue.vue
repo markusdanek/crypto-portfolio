7ec8ac7fd530a8bc55390d4d0c5ff3a717c7a1a0
Markus Danek <markus.danek-cic@ibm.com>
2018-02-16 12:05:48 +0100

    Add cumulated sum for today for grouped crypto

--- src/components/CoinValue.vue
+++ src/components/CoinValue.vue
@@ -25,17 +25,23 @@
           coinTitle.push(title)
         }
 
-        let sum = [];
+        let sumPurchase = [];
+        let sumToday = [];
         for (var i = 0; i < coinTitle.length; i++) {
           console.log(coinTitle[i]);
-          let sumTemp = [];
+          let sumTempPurchase = [];
+          let sumTempToday = [];
           for (var j = 0; j < this.test1[coinTitle[i]].length; j++) {
             let currentTitle = this.test1[coinTitle[i]][j].title;
             let purchaseValue = this.test1[coinTitle[i]][j].purchaseValueFiat;
-            sumTemp.push(purchaseValue);
-            sum = sumTemp.reduce((pv, cv) => pv+cv, 0);
+            let priceToday = this.test1[coinTitle[i]][j].priceToday * this.test1[coinTitle[i]][j].amount;
+            sumTempPurchase.push(purchaseValue);
+            sumTempToday.push(priceToday);
+            sumPurchase = sumTempPurchase.reduce((pv, cv) => pv+cv, 0);
+            sumToday = sumTempToday.reduce((pv, cv) => pv+cv, 0);
           }
-          console.log("Purchase value in USD: ", sum);
+          console.log("PurchaseValueUSD: ", sumPurchase);
+          console.log("TodayValueUSD: ", sumToday);
         }
       }
     },
