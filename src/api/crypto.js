d6948e72d578b09fc7896330066a6455765be4f9
Markus Danek <markus.danek-cic@ibm.com>
2018-05-08 14:45:02 +0200

    Call cryptocompare api directly instead nodejs app

--- src/api/crypto.js
+++ src/api/crypto.js
@@ -1,103 +1,102 @@
 import Vue from 'vue';
-import { getJsonBody } from '../helpers/utils';
+import {getJsonBody} from '../helpers/utils';
 
+const ccUrl = 'https://min-api.cryptocompare.com/data'
 const apiUrl = 'https://cryptocoin-api.herokuapp.com'
-const devUrl = 'http://localhost:9001'
 
 export function getByUrl(url) {
-    return new Promise((resolve, reject)=>{
-        Vue.http.get(url).then((res)=>{
-            resolve(getJsonBody(res.body));
-        }).catch((err)=>{
-            reject(err);
-        });
+  return new Promise((resolve, reject) => {
+    Vue.http.get(url).then((res) => {
+      resolve(getJsonBody(res.body));
+    }).catch((err) => {
+      reject(err);
     });
+  });
 }
 
 export function getPrice(crypto, currency) {
-    return new Promise((resolve, reject) => {
-        Vue.http.get(apiUrl + '/price', {
-            params: {
-                crypto: crypto,
-                currency: currency
-            }
-        }).then((result) => {
-            resolve(getJsonBody(result.body[currency]));
-        }).catch((err) => {
-            reject(err);
-        });
+  return new Promise((resolve, reject) => {
+    Vue.http.get(ccUrl + '/price', {
+      params: {
+        fsym: crypto,
+        tsyms: currency
+      }
+    }).then((result) => {
+      resolve(getJsonBody(result.body[currency]));
+    }).catch((err) => {
+      reject(err);
     });
-}
-
-export function getValue(crypto, coins, currency) {
-    return new Promise((resolve, reject) => {
-        Vue.http.get(apiUrl + '/value', {
-            params: {
-                crypto: crypto,
-                coins: coins,
-                currency: currency
-            }
-        }).then((result) => {
-            resolve(JSON.parse(result.body));
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-}
-
-export function getDaily(crypto, currency) {
-    return new Promise((resolve, reject) => {
-        Vue.http.get(apiUrl + '/daily', {
-            params: {
-                crypto: crypto,
-                currency: currency
-            }
-        }).then((result) => {
-            resolve(JSON.parse(result.body));
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-}
-
-export function getMonthly(crypto, currency) {
-    return new Promise((resolve, reject) => {
-        Vue.http.get(apiUrl + '/value', {
-            params: {
-                crypto: crypto,
-                currency: currency
-            }
-        }).then((result) => {
-            resolve(JSON.parse(result.body));
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-}
-
-export function getPortfolio() {
-    return new Promise((resolve, reject) => {
-        Vue.http.get(apiUrl + '/portfolio', {
-        }).then((result) => {
-            resolve(JSON.parse(result.body));
-        }).catch((err) => {
-            reject(err);
-        });
-    });
+  });
 }
 
 export function getPriceForTimestamp(crypto, day, currency) {
-    return new Promise((resolve, reject) => {
-      Vue.http.get(apiUrl + '/historicprice', {
-          params: {
-              crypto: crypto,
-              day: day,
-              currency: currency
-          }
-        }).then((result) => {
-            resolve(JSON.parse(result.body[currency]));
-        }).catch((err) => {
-            reject(err);
-        });
+  return new Promise((resolve, reject) => {
+    Vue.http.get(ccUrl + '/pricehistorical', {
+      params: {
+        fsym: crypto,
+        day: day,
+        tsyms: currency
+      }
+    }).then((result) => {
+      resolve(JSON.parse(result.body[crypto][currency]));
+    }).catch((err) => {
+      reject(err);
     });
+  });
+}
+
+export function getValue(crypto, coins, currency) {
+  return new Promise((resolve, reject) => {
+    Vue.http.get(apiUrl + '/value', {
+      params: {
+        crypto: crypto,
+        coins: coins,
+        currency: currency
+      }
+    }).then((result) => {
+      resolve(JSON.parse(result.body));
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
+export function getDaily(crypto, currency) {
+  return new Promise((resolve, reject) => {
+    Vue.http.get(apiUrl + '/daily', {
+      params: {
+        crypto: crypto,
+        currency: currency
+      }
+    }).then((result) => {
+      resolve(JSON.parse(result.body));
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
+export function getMonthly(crypto, currency) {
+  return new Promise((resolve, reject) => {
+    Vue.http.get(apiUrl + '/value', {
+      params: {
+        crypto: crypto,
+        currency: currency
+      }
+    }).then((result) => {
+      resolve(JSON.parse(result.body));
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
+export function getPortfolio() {
+  return new Promise((resolve, reject) => {
+    Vue.http.get(apiUrl + '/portfolio', {}).then((result) => {
+      resolve(JSON.parse(result.body));
+    }).catch((err) => {
+      reject(err);
+    });
+  });
 }
